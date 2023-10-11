585759acab68cb01f1eb8a6e916419dc8e43c2fe
cristianTeknoir <cristian.manaila@teknoir.ai>
2023-10-11 21:18:57 +0300

    zapier-embedded

--- src/zapier-embedded.js
+++ src/zapier-embedded.js
@@ -1,16 +1,12 @@
 module.exports = function(RED) {
     function ZapierEmbedNode(config) {
-        RED.nodes.createNode(this, config);
-        
-        var node = this;
-
-        // Handle incoming messages
-        this.on('input', function(msg) {
-            // For now, just pass the message through.
-            // You can add more logic here if needed.
-            node.send(msg);
-        });
+      RED.nodes.createNode(this, config);
+      var node = this;
+      node.on('input', function(msg) {
+        // Custom logic here
+        node.send(msg);
+      });
     }
-
     RED.nodes.registerType("zapier-embed", ZapierEmbedNode);
-}
+  };
+  
